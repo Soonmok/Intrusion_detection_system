b3f21105fd12e9f7e8ff17831fc2e1c4d8731c42
Soonmok <tnsahr2580@gmail.com>
2019-04-01 20:29:17 +0900

    added names of columns

--- data_processing.py
+++ data_processing.py
@@ -1,19 +1,49 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 def load_data(filenames):
     datasets = []
     for filename in filenames:
-        dataset = pd.read_csv(filename, sep=',', header=None)
+        names = ['duration', 'protocol_type', 'service', 'flag', 'src_byte',
+                  'dst_bytes', 'land', 'wrong_fragment', 'urgent', 'hot',
+                  'num_failed_logins', 'logged_in', 'num_compromised',
+                   'root_shell', 'su_attempted', 'num_root',
+                   'num_file_creations', 'num_shells', 
+                  'num_access_files', 'num_outbound_cmds', 'is_host_login', 
+                  'is_guest_login', 'count', 'srv_count', 'serror_rate',
+                  'srv_serror_rate', 'rerror_rate', 'srv_rerror_rate', 
+                  'same_srv_rate', 'diff_srv_rate', 'srv_diff_host_rate',
+                  'dst_host_count', 'dst_host_srv_count',
+                   'dst_host_same_srv_rate', 'dst_host_diff_srv_rate',
+                  'dst_host_same_src_port_rate', 'dst_host_srv_diff_host_rate',
+                  'dst_host_serror_rate', 'dst_host_srv_serror_rate',
+                  'dst_host_rerror_rate', 'dst_host_srv_rerror_rate',
+                  'target', 'dummy']
+        dataset = pd.read_csv(filename, sep=',', header=None, names=names)
         datasets.append(dataset)
     datasets = pd.concat(datasets, axis=0, ignore_index=True)
     return datasets
 
 def process_data(datasets):
-    index_to_category = [1, 2, 3, 6, 11, 20, 21, 41]
-    datasets[index_to_category] = datasets[index_to_category].astype('category')
-    data = datasets.iloc[:, :41]
-    labels = datasets.iloc[:, 41]
-    return data, labels
+    label_encoder = LabelEncoder()
+    index_to_category = ['protocol_type', 'service', 'flag', 'land',
+                         'logged_in', 'is_host_login', 'is_guest_login',
+                         'target']
+    encode_label(datasets, index_to_category)
+    indices = range(len(datasets))
+    train_indices, dev_indices = train_test_split(indices, shuffle=True)
+    print("loading total data {} train data {}, dev_data {}".format(
+        len(datasets), len(train_indices), len(dev_indices)))
+    train_data = datasets.iloc[train_indices, :41]
+    train_labels = datasets.iloc[train_indices, 41]
+    dev_data = datasets.iloc[dev_indices, :41]
+    dev_labels = datasets.iloc[dev_indices, 41]
+    return train_data, train_labels, dev_data, dev_labels
     
+def encode_label(data, index_to_category):
+    for index in index_to_category:
+        le = LabelEncoder().fit(data[index])
+        data[index] = le.transform(data[index])
--- data_processing_test.py
+++ data_processing_test.py
@@ -5,14 +5,14 @@ def test_load_data():
     mock_data = load_data(filenames)
     assert len(mock_data) == 20
 
-def process_data_test():
+def test_process_data():
     filenames = ['./mock_data.txt', './mock_data2.txt']
     mock_datasets = load_data(filenames)
-    data, labels = process_data(mock_datasets)
-    assert data.shape == (20, 41)
-    assert labels.shape == (20, )
+    train_data, train_labels, _, _ = process_data(mock_datasets)
+    assert train_data.shape == (15, 41)
+    assert train_labels.shape == (15, )
 
 if __name__=="__main__":
     test_load_data()
-    process_data_test()
+    test_process_data()
     print("test is all passed")
